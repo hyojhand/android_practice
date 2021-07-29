c53e8a5092a1600c36e643b62cd2dc563a199200
hyojhand <sonjw5128@gmail.com>
2021-07-29 16:42:30 +0900

    [UPDATE] error code update complete

--- practice1/app/src/main/java/com/example/practice1/CustomPagerAdapter.kt
+++ practice1/app/src/main/java/com/example/practice1/CustomPagerAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.practice1
 
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
--- practice1/app/src/main/java/com/example/practice1/MainActivity.kt
+++ practice1/app/src/main/java/com/example/practice1/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.practice1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practice1.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         val textList = listOf("뷰A","뷰B","뷰C","뷰D")
         val customAdapter = CustomPagerAdapter()
         customAdapter.textList = textList
+
         binding.viewPager.adapter = customAdapter
         val tabTitles = listOf("View A","View B","View C","View D")
         TabLayoutMediator(binding.tabLayout, binding.viewPager){tab,position ->
